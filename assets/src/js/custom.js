c610224c666659948888acc883c21b12e79136de
Navruz Zubaydullayev <zubaydullayevnavruz@gmail.com>
2021-06-30 10:55:38 +0500

    finish

--- assets/src/js/custom.js
+++ assets/src/js/custom.js
@@ -94,62 +94,22 @@ $(function(){
     },
   });
 
-
-
-  $(document).ready(function () {
-      $('#vmap').vectorMap(
-        {
-        map: 'asia_en',
-        borderColor: '#818181',
-        borderOpacity: 0.5,
-        color: '#BDDCF0',
-        hoverColor: '#228BCD',
-        selectedColor: '#228BCD',
-        backgroundColor: 'none',
-        hoverOpacity: 0.7,
-        selectedColor: '#666666',
-        showTooltip: true,
-        showTooltip: true,
-        borderWidth: 1,
-        enableZoom: false,
-        hoverOpacity: null,
-        values: sample_data,
-        scaleColors: ['#C8EEFF', '#006491'],
-        normalizeFunction: 'linear',
-        selectedRegions: ['uzbekistan'],
-      }
-    );
+  var swiper = new Swiper(".mySwiper4", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
   });
 
-
-
-  
-  $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-
-
-
-
-  var swiper = new Swiper(".mySwiper1", {
+  var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerGroup: 4,
-    loop: true,
-    loopFillGroupWithBlank: true,
+    centeredSlides: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
   });
-
 
 
   var swiper = new Swiper(".mySwiper3", {
@@ -160,51 +120,40 @@ $(function(){
 
 
 
+  $('#nav-icon1').on('click', function(){
+    $(this).toggleClass('open');
+    $('.hamburger-menu').toggleClass("active");
+  });
+  
+  $('.hamburger-menu li').on('click', function(){
+    $('.hamburger-menu').removeClass('active')
+    $('#nav-icon1').removeClass('open')
+  });
 
 
-const labels = document.querySelectorAll(".accordion-item__label");
-const tabs = document.querySelectorAll(".accordion-tab");
-alert(tabs);
-function toggleShow() {
-	const target = this;
-	const item = target.classList.contains("accordion-tab")
-		? target
-		: target.parentElement;
-	const group = item.dataset.actabGroup;
-	const id = item.dataset.actabId;
 
-	tabs.forEach(function(tab) {
-		if (tab.dataset.actabGroup === group) {
-			if (tab.dataset.actabId === id) {
-				tab.classList.add("accordion-active");
-			} else {
-				tab.classList.remove("accordion-active");
-			}
-    }
-    
-	});
-
-	labels.forEach(function(label) {
-		const tabItem = label.parentElement;
-
-		if (tabItem.dataset.actabGroup === group) {
-			if (tabItem.dataset.actabId === id) {
-				tabItem.classList.add("accordion-active");
-			} else {
-				tabItem.classList.remove("accordion-active");
-			}
-		}
-	});
-}
-
-labels.forEach(function(label) {
-	label.addEventListener("click", toggleShow);
+$(document).ready(function () {
+  $.mask.definitions['9'] = '';
+  $.mask.definitions['n'] = '[0-9]';
+  $(function () {
+      $("#numberId").mask("+998 nn nnn nn nn");
+      $("#numberId1").mask("+998 nn nnn nn nn");
+      // $("#phone-input2").mask("+998 nn nnn nn nn");
+      // $("#phone-input3").mask("XXXX XXXX XXnn nnnn");
+      // $("#phone-input4").mask("nn/nn");
+      // $("#phone-input5").mask("nnnn");
+  });
 });
 
-tabs.forEach(function(tab) {
-	tab.addEventListener("click", toggleShow);
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 8000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
 });
-
-
-
-
+  
