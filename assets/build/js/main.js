339f623d7f2081ccc527941785b09085f202ee31
Navruz Zubaydullayev <zubaydullayevnavruz@gmail.com>
2021-07-01 18:34:38 +0500

    the end

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -18109,15 +18109,3 @@ $(document).ready(function () {
       // $("#phone-input5").mask("nnnn");
   });
 });
-
-$('.count').each(function () {
-    $(this).prop('Counter', 0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 8000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
--- assets/src/js/custom.js
+++ assets/src/js/custom.js
@@ -145,15 +145,7 @@ $(document).ready(function () {
   });
 });
 
-$('.count').each(function () {
-    $(this).prop('Counter', 0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 8000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+
   
+
+
